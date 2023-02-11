4d62a18d371bd3b1101e619fcf9115f723a6ab63
Anthony John <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-02-11 21:45:29 +0800

    Version 1

---
+++ js-scripts/general.js
@@ -0,0 +1,19 @@
+let logoButton = document.querySelector("#logo-button");
+
+function redirectHome() {
+    location.href = "index.html";
+}
+
+function redirectTeam() {
+    location.href = "teampage.html"
+}
+
+function redirectProject() {
+    location.href = "projectPage.html"
+}
+
+function redirectContact() {
+    location.href = "contactpage.html"
+}
+
+
