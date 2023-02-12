30b1ff93626a478ccf701060204195ae516f7d72
Anthony John <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-02-12 20:09:28 +0800

    Fill the placeholders

---
+++ js-scripts/motto.js
@@ -0,0 +1,23 @@
+document.querySelector("#aj").addEventListener('click', function() {
+    alert('Insert Motto');
+});
+
+document.querySelector("#jez").addEventListener('click', function() {
+    alert('Insert Motto');
+});
+
+document.querySelector("#sheree").addEventListener('click', function() {
+    alert('Insert Motto');
+});
+
+document.querySelector("#wayne").addEventListener('click', function() {
+    alert('Insert Motto');
+});
+
+document.querySelector("#reggie").addEventListener('click', function() {
+    alert('Insert Motto');
+});
+
+document.querySelector("#abril").addEventListener('click', function() {
+    alert('Insert Motto');
+});
