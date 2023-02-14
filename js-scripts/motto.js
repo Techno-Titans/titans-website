90715cdf44194e984d5caaafbe3a08b1bc31b751
Anthony John <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-02-14 22:41:21 +0800

    Unfinished mottos

--- js-scripts/motto.js
+++ js-scripts/motto.js
@@ -1,23 +1,23 @@
-document.querySelector("#aj").addEventListener('click', function() {
-    alert('Insert Motto');
+document.querySelector("#aj").addEventListener('click', () => {
+    alert("'Ad Astra per Aspera'");
 });
 
-document.querySelector("#jez").addEventListener('click', function() {
-    alert('Insert Motto');
+document.querySelector("#jez").addEventListener('click', () => {
+    alert("'Our life is what thoughts make it.'");
 });
 
-document.querySelector("#sheree").addEventListener('click', function() {
-    alert('Insert Motto');
+document.querySelector("#sheree").addEventListener('click', () => {
+    alert("'Insert Motto'");
 });
 
-document.querySelector("#wayne").addEventListener('click', function() {
-    alert('Insert Motto');
+document.querySelector("#wayne").addEventListener('click', () => {
+    alert("'Sometimes you never realize the value of a moment until becomes a memory.'");
 });
 
-document.querySelector("#reggie").addEventListener('click', function() {
-    alert('Insert Motto');
+document.querySelector("#reggie").addEventListener('click', () => {
+    alert("'Carpe diem'");
 });
 
-document.querySelector("#abril").addEventListener('click', function() {
+document.querySelector("#abril").addEventListener('click', () => {
     alert('Insert Motto');
 });
